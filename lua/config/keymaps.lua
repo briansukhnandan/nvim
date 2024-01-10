4f9156c9774592a93633caa05969b1ca423b4393
Brian Sukhnandan <brian@arch.co>
2024-01-10 00:08:42 -0500

    LazyVim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,29 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap.set
+local silent = { silent = true }
+local expr_opts = { expr = true, silent = true }
+
+function Map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  keymap(mode, lhs, rhs, options)
+end
+
+-- Don't yank on delete char
+Map("n", "x", '"_x', silent)
+Map("n", "X", '"_X', silent)
+Map("v", "x", '"_x', silent)
+Map("v", "X", '"_X', silent)
+
+-- Remove highlights
+Map("n", "<ESC>", ":noh<CR><CR>", silent)
+
+-- Tabs
+Map("n", "<leader>nt", "<cmd>tabnew<cr>", { desc = "New Tab" })
+Map("n", "<C-z>l", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+Map("n", "<C-z>h", "<cmd>tabprev<cr>", { desc = "Previous Tab" })
+Map("n", "<leader>ct", "<cmd>bd<cr>", { desc = "Close Tab" })
