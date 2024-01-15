4d614c76e41915392ccd90e22566f16e41cb3c08
Brian Sukhnandan <brian@arch.co>
2024-01-14 20:00:15 -0500

    make catppuccin-mocha the default

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -17,12 +17,12 @@ return {
     },
   },
 
-  -- Set default colorscheme to dracula.
   { "Mofiqul/dracula.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "catppuccin-mocha",
     },
   },
 
