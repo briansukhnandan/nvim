7e52b790d0c6b71886ad4adc1630f611f433ba3b
Brian Sukhnandan <brian@arch.co>
2024-01-28 22:41:57 -0500

    catppuccin overrides

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -12,8 +12,23 @@ return {
     },
   },
 
-  { "Mofiqul/dracula.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function ()
+      require("catppuccin").setup({
+        color_overrides = {
+          -- Not a fan of the default frappe background color
+          -- however the rest of the theming is nice.
+          frappe = {
+            base = "#363636",
+            mantle = "#404040",
+            crust = "#4d4d4d",
+          },
+        }
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
