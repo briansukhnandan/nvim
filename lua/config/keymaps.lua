31aeba71942e41a863bb2cf645850d266ef997e3
Brian Sukhnandan <briansukhnandan@gmail.com>
2024-02-11 15:38:14 -0500

    some mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,18 +13,42 @@ function Map(mode, lhs, rhs, opts)
   keymap(mode, lhs, rhs, options)
 end
 
+-- Disables arrow keys in Normal mode 
+Map("n", "<Left>", "<Nop>")
+Map("n", "<Up>", "<Nop>")
+Map("n", "<Left>", "<Nop>")
+Map("n", "<Right>", "<Nop>")
+
 -- Don't yank on delete char in normal mode.
 -- We still want to retain in visual.
 Map("n", "x", '"_x', silent)
 Map("n", "X", '"_X', silent)
 
+-- I also don't like yanking when deleting.
+Map("n", "d", '"_d', silent)
+
 -- Remove highlights
 Map("n", "<ESC>", ":noh<CR><CR>", silent)
 
-Map("n", "<C-/>", "i", silent)
-
--- Ctrl+c escapes insert mode.
+-- Alternative: Ctrl+c escapes insert mode.
 Map("i", "<C-c>", "<ESC>", silent)
+
+-- Scrolling - Personally I think 5 lines down is 
+-- reasonable esp when scrubbing through large files.
+-- 
+-- I find it comfortable to use W and S for associativity 
+-- with moving up/down since I play games, so I use alt + w/s 
+-- for scrolling 5.
+--
+-- I also don't really mind ctrl+e/y for scrolling which is 
+-- the default vim scroll bindings so i use alt+e/y for 
+-- scrolling 3.
+Map("n", "<A-s>", "<C-e> 5<C-e>")
+Map("n", "<A-w>", "<C-y> 5<C-y>")
+
+Map("n", "<A-e>", "<C-e> 3<C-e>")
+Map("n", "<A-y>", "<C-y> 3<C-y>")
+
 
 -- Tabs (and buffers)
 --
@@ -52,9 +76,12 @@ Map("n", "<leader>gd", "<cmd>Glance definitions<cr>")
 Map("n", "<leader>gy", "<cmd>Glance type_definitions<cr>")
 Map("n", "<leader>gm", "<cmd>Glance implementations<cr>")
 
+-- Goes to the matching grouping (Shift+5)
+Map("n", "M", "%")
+
 -- MISC --
 
 -- Random nick nacks i have for day to day.
 -- The below makes a basic JS/TS group comment block
 -- and leaves in insert mode
-Map("n", "tsc", "i/**<cr><Space>*<cr><Space>*/")
+Map("n", "tsc", "i/****/")
