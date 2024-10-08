9caecea8531090a5224ef1b0bb9db16c7ad0f110
Brian Sukhnandan <brian@arch.co>
2024-10-07 23:52:33 -0400

    more treesitter stuff

--- lua/treesitter_conf.lua
+++ lua/treesitter_conf.lua
@@ -3,3 +3,62 @@ require'treesitter-context'.setup{
   line_numbers = true,
   multiline_threshold = 3, -- Maximum number of lines to show for a single context
 }
+
+require("nvim-treesitter.configs").setup {
+  -- You can put "all" to make treesitter install every available parser
+  ensure_installed = {
+    "c",
+    "vim",
+    "lua",
+    "rust",
+    "javascript",
+    "typescript",
+    "bash",
+    "diff",
+    "gitcommit",
+    "gitignore",
+    "git_rebase",
+    "gitattributes"
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  playground = {
+    enabled = true,
+    update_time = 25,
+    persist_queries = false,
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    }
+  },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = {'markdown'},
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
