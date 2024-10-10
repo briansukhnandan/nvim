d9ab860d43f84c7215496558a898d3adc0be6574
Brian Sukhnandan <brian@arch.co>
2024-10-10 01:03:43 -0400

    enable treesitter indent

--- lua/treesitter_conf.lua
+++ lua/treesitter_conf.lua
@@ -45,6 +45,10 @@ require("nvim-treesitter.configs").setup {
     }
   },
 
+  indent = {
+    enable = true,
+  },
+
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
