5b4adc9834967d91bdc43062202d7a68997c2cd2
Brian Sukhnandan <briansukhnandan@gmail.com>
2024-10-12 02:28:01 -0400

    add keybinds for pasting in terminal/insert mode

--- lua/nvim_conf.lua
+++ lua/nvim_conf.lua
@@ -18,6 +18,14 @@ if vim.g.neovide then
   vim.g.neovide_window_blurred = true
   vim.g.neovide_cursor_vfx_mode = "torpedo"
 
+  -- Neovide specific keybinds
+  -- Paste insert ctrl+shift+v or command+v
+  vim.keymap.set('i', '<sc-v>', '<C-R>+', {noremap = true})
+  vim.keymap.set('i', '<D-v>', '<C-R>+', {noremap = true})
+
+  -- Paste in terminal mode with ctrl+shift+v
+  vim.keymap.set('t', '<sc-v>', '<C-\\><C-n>"+Pi', {noremap = true})
+
   local change_scale_factor = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
   end
