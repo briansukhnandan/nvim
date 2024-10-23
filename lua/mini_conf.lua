dcf26c66d547abb0e580d1107c2a9a63436af198
Brian Sukhnandan <brian@arch.co>
2024-10-22 23:04:53 -0400

    ignore certain filetypes for indentscope

--- lua/mini_conf.lua
+++ lua/mini_conf.lua
@@ -1,3 +1,16 @@
 require('mini.pairs').setup()
 require('mini.comment').setup()
 require('mini.indentscope').setup()
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Disable indentscope for certain filetypes",
+  pattern = {
+    "mason",
+    "toggleterm",
+    "dashboard",
+    "telescope",
+  },
+  callback = function()
+    vim.b.miniindentscope_disable = true
+  end,
+})
