50bc86284bcdcb6ade19a5494825ae27485e04f6
Brian Sukhnandan <briansukhnandan@BrianSukhnandan-MBP.local>
2025-06-16 00:22:29 -0400

    show relative path on lualine

--- lua/lualine_conf.lua
+++ lua/lualine_conf.lua
@@ -2,4 +2,15 @@ require('lualine').setup {
   options = {
     theme = 'dracula',
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        -- 0 = filename only,
+        -- 1 = relative path,
+        -- 2 = absolute path
+        path = 1
+      }
+    }
+  }
 }
